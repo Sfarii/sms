e1f65210b82a7733b463c0dd994f9471a25943dc
sfari <rami4sfari@gmail.com>
2017-07-10 04:27:58 +0100

    SMS v0.1

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -3,10 +3,9 @@ set :domain,      "inter-hannibalalliance.org"
 set :deploy_to,   "/var/www/html"
 set :app_path,    "app"
 
-set :repository,  "git@github.com:/sfari/sms.git"
+set :repository,  "https://github.com/sfari/sms.git"
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
-set :branch, "master"
 
 set :model_manager, "doctrine"
 # Or: `propel`
@@ -14,7 +13,8 @@ set :model_manager, "doctrine"
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 set  :keep_releases,  3
-set :deploy_via, :copy
+set :deploy_via,  :copy
+set :interactive_mode, false
 # Be more verbose by uncommenting the following line
 logger.level = Logger::MAX_LEVEL
 set :use_composer, true
@@ -22,12 +22,11 @@ set :use_composer, true
 set :shared_files,      ["app/config/parameters.yml"]
 set :shared_children,     [app_path + "/logs", web_path + "/images", "vendor", app_path + "/sessions"]
 #Configure your server
-set :use_sudo,      true
-set :user, "rami"
+set :use_sudo,      false
+set :user, "hamza"
 set :writable_dirs,       ["app/cache", "app/logs", "app/sessions"]
 set :webserver_user,      "www-data"
 set :permission_method,   :acl
 set :use_set_permissions, true
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
-ssh_options[:keys] = %w(/home/sfari/.ssh/id_rsa)
